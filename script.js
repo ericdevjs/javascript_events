a18283be509dadf5c2eca7c2f79a85810f875e25
Eric Ouellette <ericsmusic@Erics-Mac-Pro-2.local>
2018-01-17 13:25:51 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+var btn = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var items = document.querySelectorAll("li");
+var listContent = document.createTextNode(input.value);
+
+// TEST CODE START
+
+function inputLength() {
+    return input.value.length
+}
+
+function createListElement() {
+    // var li = document.createElement("li");
+    ul.innerHTML = "<li><span><i class="fa fa-trash"></i></span>" + listContent + "</li>";
+    // var listContent = document.createTextNode(input.value);
+    // li.appendChild(document.createTextNode(input.value));
+    // ul.appendChild(li);
+    li.addEventListener("click", classToggle);
+    input.value = "";
+  }
+
+function addListAfterKeypress() {
+    if(inputLength() > 0 && event.keyCode === 13) {
+      createListElement();
+    }
+}
+
+function classToggle() {
+  this.classList.toggle("done");
+}
+
+for(var i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", classToggle);
+}
+
+// btn.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+
+// TEST CODE END
+
+// function inputLength() {
+//     return input.value.length
+// }
+//
+// function createListElement() {
+//     // var li = document.createElement("li");
+//     ul.innerHTML = "<li><span><i class="fa fa-trash"></i></span>" + listContent + "</li>";
+//     // var listContent = document.createTextNode(input.value);
+//     // li.appendChild(document.createTextNode(input.value));
+//     // ul.appendChild(li);
+//     li.addEventListener("click", classToggle);
+//     input.value = "";
+//   }
+// function addListAfterClick() {
+//     if(inputLength() > 0) {
+//       createListElement();
+//     }
+// }
+//
+// function addListAfterKeypress() {
+//     if(inputLength() > 0 && event.keyCode === 13) {
+//       createListElement();
+//     }
+// }
+//
+// function classToggle() {
+//   this.classList.toggle("done");
+// }
+//
+// for(var i = 0; i < items.length; i++) {
+//     items[i].addEventListener("click", classToggle);
+// }
+//
+// // btn.addEventListener("click", addListAfterClick);
+//
+// input.addEventListener("keypress", addListAfterKeypress);
